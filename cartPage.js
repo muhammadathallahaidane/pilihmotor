2c2d7f40517f40d2ec69bcaf2ceafbe5759a4c25
muhammadathallahaidane <talaaidane@gmail.com>
2025-04-10 13:10:55 +0700

    10/04/2025 13:10

--- cartPage.js
+++ cartPage.js
@@ -1,10 +1,10 @@
 function pilihDelivery(answer) {
-    if (answer.value === "dealer") {
+    if (answer === "dealer") {
         document.getElementById("dealer").classList.remove("displayNone")
         if (!document.getElementById("home").classList.contains("displayNone")) {
             document.getElementById("home").classList.add("displayNone")
         }
-    } else if (answer.value === "home") {
+    } else if (answer === "home") {
         document.getElementById("home").classList.remove("displayNone")
         if (!document.getElementById("dealer").classList.contains("displayNone")) {
             document.getElementById("dealer").classList.add("displayNone")
